3b85071cba1d5a35c5831a4b66cf50359c03a7c0
jeff-techstension <157553534+jeff-techstension@users.noreply.github.com>
2024-08-15 13:53:53 -0500

    added card grid layout (#1)
    Co-authored-by: Jeffrey <jeff@jjakt.monster>

---
+++ Techstension.FilterEase/FilterEaseGrid.razor.cs
@@ -0,0 +1,130 @@
+﻿using FilterEase.Data;
+
+namespace Techstension.FilterEase
+{
+    public partial class FilterEaseGrid
+    {
+        public List<CardDto> Cards =
+    [
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Marvelous 3",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Pride",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Triumph",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Grand",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "Classic 56D",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "County Aire",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Carolina 1454",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Image",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Good Vibrations",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "Still The One",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Diamond",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Lewis",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Washington",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Savannah",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Marvel",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Satisfaction",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        },
+        new() {
+            Id = Guid.NewGuid().ToString(),
+            Title = "The Excitement",
+            SubTitle = "Something Catchy",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            Image = "https://picsum.photos/400/300"
+        }
+    ];
+    }
+}
---
+++ Techstension.FilterEase/FilterEaseItem.razor.cs
@@ -0,0 +1,11 @@
+﻿using FilterEase.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Techstension.FilterEase
+{
+    public partial class FilterEaseItem
+    {
+        [Parameter]
+        public CardDto? Card { get; set; }
+    }
+}
