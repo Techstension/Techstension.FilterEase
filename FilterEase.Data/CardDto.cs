30791c5f2285be5eefb3ebbf505122991ccc4f5a
Jeffrey <jeff@jjakt.monster>
2024-08-15 13:48:12 -0500

    added card grid layout

---
+++ FilterEase.Data/CardDto.cs
@@ -0,0 +1,33 @@
+﻿namespace FilterEase.Data
+{
+    /// <summary>
+    /// Card data structure that is expected to be received from the API
+    /// </summary>
+    public record CardDto
+    {
+        /// <summary>
+        /// Primary Id for the card.
+        /// </summary>
+        public required string Id { get; init; }
+        /// <summary>
+        /// The Primary Title for the card
+        /// </summary>
+        public required string Title { get; init; }
+
+        /// <summary>
+        /// Subtitle for the card
+        /// </summary>
+        public string SubTitle { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Image url to be prominently displayed on the card,
+        /// </summary>
+        public string Image { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Description for the card. 
+        /// </summary>
+        public string Description { get; init; } = string.Empty;
+
+    }
+}
