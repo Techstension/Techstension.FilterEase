261b8f96d6c9bd6cff5c93e91c138535765ffbab
Jeffrey <jeff@jjakt.monster>
2024-08-15 20:21:57 -0500

    finished styling card

--- FilterEase.Data/CardDto.cs
+++ FilterEase.Data/CardDto.cs
@@ -29,5 +29,24 @@
         /// </summary>
         public string Description { get; init; } = string.Empty;
 
+        /// <summary>
+        /// The text to  be displayed on the primary button
+        /// </summary>
+        public required string PrimaryActionLinkText { get; init; }
+        /// <summary>
+        /// The Primary Action that you wan tthe user to take
+        /// </summary>
+        public required string PrimaryActionLinkUrl { get;init; }
+        /// <summary>
+        /// The text to  be displayed on the Secondary button
+        /// </summary>
+        public required string SecondaryActionLinkText { get; init; }
+
+        /// <summary>
+        /// An acceptable secondary action the user could take. 
+        /// For instance a link for more information.
+        /// </summary>
+        public required string SecondaryActionLinkUrl { get;init; }
+
     }
 }
